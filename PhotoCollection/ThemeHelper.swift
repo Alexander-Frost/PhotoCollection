6ba20e8789873344df6725362975ecc6ff814503
Alex <alexcc17@gmail.com>
2019-05-02 16:15:49 -0400

    Create ThemeHelper class

---
+++ PhotoCollection/ThemeHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Alex on 5/2/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThemeHelper{
+    
+    let themePreferenceKey: String = "thisIsMyKey"
+    
+    
+}
