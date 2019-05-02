2e79b8172ddb3055d082ca1b32a3e33702c8a81d
Alex <alexcc17@gmail.com>
2019-05-02 16:58:53 -0400

    Set up full storyboard

---
+++ PhotoCollection/ThemeSelectionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Alex on 5/2/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    @IBAction func selectDarkTheme(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectBlueTheme(_ sender: UIButton) {
+    }
+    
+    var themeHelper: ThemeHelper?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
