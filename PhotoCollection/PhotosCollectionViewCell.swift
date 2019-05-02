e9a30c82ba8f388a722a02d13d18eaa453115a67
Alex <alexcc17@gmail.com>
2019-05-02 17:00:12 -0400

    Add to cell class

--- PhotoCollection/PhotosCollectionViewCell.swift
+++ PhotoCollection/PhotosCollectionViewCell.swift
@@ -12,5 +12,15 @@ class PhotosCollectionViewCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var myLbl: UILabel!
     
+    var photo: Photo? {
+        didSet{
+            updateViews()
+        }
+    }
+    
+    private func updateViews(){
+        
+    }
+    
     
 }
