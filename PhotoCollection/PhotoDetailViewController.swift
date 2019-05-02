31f926e7bfed602f7f5425b6a6710e028064ffb6
Alex <alexcc17@gmail.com>
2019-05-02 19:48:52 -0400

    Add to PhotoDetail

--- PhotoCollection/PhotoDetailViewController.swift
+++ PhotoCollection/PhotoDetailViewController.swift
@@ -8,40 +8,81 @@
 
 import UIKit
 
-class PhotoDetailViewController: UIViewController {
-
+class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet var myTextField: UITextField!
     @IBOutlet var imageView: UIImageView!
     
     @IBAction func savePhoto(_ sender: UIBarButtonItem) {
+        guard let image = imageView.image,
+            let imageData = image.pngData(),
+            let text = myTextField.text else {return}
+        
+        // not sure
+        if let myPhoto = photo {
+            photoController?.update(title: text, data: imageData, photo: myPhoto)
+            navigationController?.popToRootViewController(animated: true)
+        } else {
+            photoController?.create(photo: Photo(imageData: image.pngData()!, title: text))
+            navigationController?.popToRootViewController(animated: true)
+        }
     }
+    
     @IBAction func addPhoto(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        
+        present(imagePickerController, animated: true, completion: nil)
     }
+    
     var photoController: PhotoController?
     var photo: Photo?
     var themeHelper: ThemeHelper?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
+    
+    
     func setTheme(){
+        guard let myTheme = themeHelper?.themePreference else { return }
         
+        if myTheme == "Dark" {
+            view.backgroundColor = .gray
+        } else if myTheme == "Blue" {
+            view.backgroundColor = .blue
+        }
     }
     
     private func updateViews(){
-        
+        setTheme()
+        guard let photo = photo else { return }
+        imageView.image = UIImage(data: photo.imageData)
+        myTextField.text = photo.title
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
