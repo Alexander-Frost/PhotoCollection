037d295a04bbeaae7a07f369946c303496dc53b0
Alex <alexcc17@gmail.com>
2019-05-02 16:40:23 -0400

    Add CollectionViewController

---
+++ PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Alex on 5/2/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
